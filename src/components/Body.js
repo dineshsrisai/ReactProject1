fe51579d371a0a03ec0451471160de8f4d643f06
dineshsrisai <dineshsrisai1@gmail.com>
2025-08-20 21:40:09 +0530

    Modified

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,38 +2,35 @@ import RestaurantCard from "./RestaurantCard";
 import {useState , useEffect} from "react";
 
 const Body = () => {
-  const [listOfRestaurants,setListOfRestaurant] = useState([]);
+  const [listOfRestaurant,setRestaurantList] = useState([]);
 
   useEffect(()=>{
     fetchData();
-  },[]);
+  });
 
-  const fetchData = async () => {
+  const fetchData = async()=>{
     const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=16.5174552&lng=81.7253415&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
     const json = await data.json();
-    console.log(json);
-    setListOfRestaurant(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants); 
-  };
+    setRestaurantList(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+  }
 
-  if(listOfRestaurants.length === 0){
-    return <h1>Loading...</h1>
+  if(listOfRestaurant.length==0){
+    return <h1>Loading...</h1>;
   }
 
   return (
     <div className="body">
       <div className="filter">
-        <button className="filter-btn" 
-          onClick={() => {
-          const filteredList = listOfRestaurants.filter(
-            (res) => res.info.avgRating >= 4
-          );
-            setListOfRestaurant(filteredList);
-          }}
-        >Filter</button>
+        <div className="filter-btn">
+            <button onClick={()=>{
+                const filteredList = listOfRestaurant.filter((res) => res.info.avgRating > 4);
+                setRestaurantList(filteredList);
+            }}>Filter</button>
+        </div>
       </div>
       <div className="res-container">
         {
-          listOfRestaurants.map(restaurant => <RestaurantCard key={restaurant.info.id} resData={restaurant} />)
+            listOfRestaurant.map(restaurant => <RestaurantCard key={restaurant.info.id} resData={restaurant}/>)
         }
       </div>
     </div>
